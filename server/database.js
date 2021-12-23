5b6f4dc4d902aca711d4456c0c9b7ad575154181
Walid <walid_douri@hotmail.com>
2021-12-23 01:25:16 -0500

    add property

--- server/database.js
+++ server/database.js
@@ -48,13 +48,13 @@ exports.getUserWithId = getUserWithId;
 
 /**
  * Add a new user to the database.
- * @param {{name: string, password: string, email: string}} user
+ * @param {{name, password, email}} user
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser = function (user) {
   const { name, email, password } = user;
   return pool
-    .query(`INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *;`,
+    .query(`INSERO users (name, email, password) VALUES ($1, $2, $3) RETURNING *;`,
       [name, email, password])
     .then(result => {
       result.rows[0];
@@ -174,9 +174,45 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function (property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  const queryParams = [
+    property.owner_id,
+    property.title,
+    property.description,
+    property.thumbnail_photo_url,
+    property.cover_photo_url,
+    property.cost_per_night,
+    property.parking_spaces,
+    property.number_of_bathrooms,
+    property.number_of_bedrooms,
+    property.country, property.street,
+    property.city,
+    property.province,
+    property.post_code];
+  const queryString = `INSERT INTO properties(
+  owner_id,
+  title,
+  description,
+  thumbnail_photo_url,
+  cover_photo_url,
+  cost_per_night,
+  street,
+  city,
+  province,
+  post_code,
+  country,
+  parking_spaces,
+  number_of_bathrooms,
+  number_of_bedrooms) 
+  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) 
+  RETURNING *`;
+
+  return pool.query(queryString, queryParams)
+    .then((results) => {
+      results.rows[0];
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+
 }
 exports.addProperty = addProperty;
